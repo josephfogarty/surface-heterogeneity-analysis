68d9cb79502dfae74747e711efbc79b3155c173e
Joseph Fogarty <36773696+jjf1218@users.noreply.github.com>
2020-06-15 16:21:55 -0400

    added map comparison

--- quantify_het/analyze_int_scale.py
+++ quantify_het/analyze_int_scale.py
@@ -56,7 +56,40 @@ df = pd.concat(pd_list,ignore_index=True)
 
 #### analyze data ####
 
+# get map data, for example
 df_maps = df.loc[df['pattern'] == 'T_s_remote_ice']
+
+# scale up
+# in this case, to the 10km by 10km domain for SHEBA maps
+reso = 128
+conv = 10000/reso
+df_maps['lp'] = conv*df_maps['lp']
+
+
+# split into x and y
+df_maps_x = df_maps[df_maps.filename.str.contains('_x',case=False)]
+df_maps_y = df_maps[df_maps.filename.str.contains('_y',case=False)]
+
+
+# plot total cooling
+labels = ['beaufo', 'esiber', 'cafram']
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+fig, ax = plt.subplots(figsize=(8,5))
+rects1 = ax.bar(x + width/2, df_maps_x['lp'], width, label='x')
+rects2 = ax.bar(x - width/2, df_maps_y['lp'], width, label='y')
+ax.set_ylabel(r'$L_p$ (m)')
+ax.set_title('Integral Length Scale of Different Maps')
+ax.set_xticks(x)
+#ax.axhline(y=0,xmin=0,color='k')
+ax.set_xticklabels(labels)
+ax.legend()
+
+# save fig
+sp = os.path.join(root,'results','int_het_scale_comparison','COMP_map.png')
+fig.savefig(sp)
+
+
 
 
 
