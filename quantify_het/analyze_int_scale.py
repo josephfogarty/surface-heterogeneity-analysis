cfbedd274a75b7380c81209a6b8824e58dd291fd
Joseph Fogarty <36773696+jjf1218@users.noreply.github.com>
2020-06-15 14:39:49 -0400

    new code
    analyse_int_scale is a new code that will take the csv results and plot comparisons

---
+++ quantify_het/analyze_int_scale.py
@@ -0,0 +1,56 @@
+"""
+A code to plot and compare integral heterogeneity length scales
+"""
+
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import glob
+
+# reset matplotlib to defalt settings
+import matplotlib as mpl
+mpl.rcParams.update(mpl.rcParamsDefault)
+
+# import constants and funcs by going up one, importing, then going back down
+os.chdir(os.path.join("D:",os.sep,"surface-heterogeneity-analysis"))
+from constants import cnst
+import funcs as fn
+os.chdir(os.path.join("create_surfaces"))
+
+# define constants - these are changed in class cnst in constants.py
+label = cnst.label
+root = cnst.root
+
+# clear all plots
+plt.close('all')
+
+#### obtain data ####
+
+# create dataframe from all csvs in the load path
+headers = ['pattern','lp']
+lp = os.path.join(root,'results','int_het_scale_txts')
+
+pd_list = []
+
+# load in each pandas dataframe
+for csv_file in os.listdir(lp):
+    if csv_file.endswith('.csv'):
+        
+        # import filename
+        print(f'\n  Importing {csv_file}')
+        
+        # read to pandas
+        df = pd.read_csv(os.path.join(lp,'checkerboard_noise_y.csv'),names=headers)
+        
+        # add filename column
+        df['filename']=f'{csv_file[:-4]}'
+        
+        # add to df list
+        pd_list.append(df)
+
+# now concatenate
+df = pd.concat(pd_list,ignore_index=True)        
+
+#### analyze data ####
